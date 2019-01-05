4863acfce7e41d385067afaa37381bed58217198
ppaulis <ppaulis@gmail.com>
2019-01-05 23:16:34 +0100

    bugfix

--- src/Tools/PubSub/Adapter/PubNubAdapterFactory.php
+++ src/Tools/PubSub/Adapter/PubNubAdapterFactory.php
@@ -10,7 +10,7 @@ class PubNubAdapterFactory implements FactoryInterface
 {
     public function __invoke(\Interop\Container\ContainerInterface $container, $requestedName, array $options = null)
     {
-        $pubNubConfig = $container->get('aws_config');
+        $pubNubConfig = $container->get('pubsub_config');
 
         $pnConfiguration = new PNConfiguration();
         $pnConfiguration
